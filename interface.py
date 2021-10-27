f30a856e23f7f4ae15978f4fc4ebc110e3d14f80
LucasAquino09/ <lucasaqs09@gmail.com>
2021-10-27 16:02:37 -0300

    New Version - Exe last Version

--- interface.py
+++ interface.py
@@ -1,31 +1,161 @@
 import PySimpleGUI as sg
 
 
-def init00():
-    sg.theme('DarkAmber')  # Add a touch of color
-    # All the stuff inside your window.
-    layout = [
-        [sg.Text('Informe o que deseja Abrir')],
-        [sg.Checkbox(' SGO    ', key='KEY_SGO'),
-         sg.Checkbox(' Linkedin ')],
-        [sg.Checkbox('Rocket '),
-         sg.Checkbox(' Facebook ')],
-        [sg.Checkbox(' Youtube'),
-         sg.Checkbox(' Instagram')],
-    ]
+class Interface:
+    def __init__(self):
 
-    # Create the Window
-    window = sg.Window('Assistente',
-                       layout,
-                       size=(600, 400),
-                       element_justification='center')
-    # Event Loop to process "events" and get the "values" of the inputs
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
-            break
+        self.sg = sg
 
-        elif values['KEY_SGO'] == True:
-            print('True')
+        self.informationPassword = {
+            'sgoAcess': ['Gabriel.cunha', 'Neto2133@'],
+            'linkedin': ['gabrielcatvalhoneto@gmail.com', 'neto2133'],
+            'email': ['gabriel.cunha@basis.com.br', 'g25443215'],
+        }
 
-        print('You entered ', values[0])
+    def main(self):
+
+        sg = self.sg
+
+        self.informationPass = {
+            'Sgo': False,
+            'Linkedin': False,
+            'Rocket': False,
+            'Email': False,
+            'Youtube': False
+        }
+
+        sg.SetOptions(background_color='#363636',
+                      text_element_background_color='#363636',
+                      element_background_color='#363636',
+                      scrollbar_color=None,
+                      input_elements_background_color='#F7F3EC',
+                      button_color=('white', '#4F4F4F'))
+
+        CheckBoxSelect = [
+            [sg.Text('Informe o que deseja Abrir\n')],
+            [
+                sg.Checkbox('SGO', key='KEY_Sgo'),
+                sg.Checkbox('Linkedin', key='KEY_Linke'),
+                sg.Checkbox('Rocket', key='KEY_Rocket'),
+                sg.Checkbox('Youtube', key='KEY_Youtube'),
+                sg.Checkbox('Email', key='KEY_Email'),
+            ],
+        ]
+        AlterSelect = [
+            [
+                sg.Button('Alterar', key='KEY_AlSgo'),
+                sg.Button('Alterar', key='KEY_AlLink'),
+                sg.Button('Alterar'),
+                sg.Button('Alterar'),
+                sg.Button('Alterar', key='KEY_AlEmail'),
+            ],
+        ]
+
+        layoutButton = [[sg.Text('\n')],
+                        [
+                            sg.Button('Iniciar',
+                                      size=(15, 2),
+                                      key='SendRequest')
+                        ]]
+
+        layout = [[sg.Column(CheckBoxSelect, element_justification='center')],
+                  [sg.Column(AlterSelect, element_justification='center')],
+                  [sg.Column(layoutButton, element_justification='center')]]
+
+        window = sg.Window('Assistente',
+                           layout,
+                           size=(600, 400),
+                           element_justification='center')
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED:
+                break
+
+            if 'KEY_AlSgo' == event:
+                print('sgoAcess')
+                self.informationPassword = self.AcessAlt('sgoAcess')
+
+            if 'KEY_AlLink' == event:
+                print('linkedin')
+                self.informationPassword = self.AcessAlt('linkedin')
+
+            if 'KEY_AlEmail' == event:
+                print('email')
+                self.informationPassword = self.AcessAlt('email')
+
+            if event == 'SendRequest':
+                break
+
+            if values['KEY_Sgo'] == True:
+                self.informationPass = {**informationPass, 'Sgo': True}
+
+            if values['KEY_Linke'] == True:
+                self.informationPass = {**informationPass, 'Linkedin': True}
+
+            if values['KEY_Rocket'] == True:
+                self.informationPass = {**informationPass, 'Rocket': True}
+
+            if values['KEY_Email'] == True:
+                self.informationPass = {**informationPass, 'Email': True}
+
+            if values['KEY_Youtube'] == True:
+                self.informationPass = {**informationPass, 'Youtube': True}
+
+        return informationPass, informationPassword
+
+    def AcessAlt(self, validationAlt):
+
+        informationPassword = self.informationPassword
+        sg = self.sg
+
+        sg.SetOptions(background_color='#363636',
+                      text_element_background_color='#363636',
+                      element_background_color='#363636',
+                      scrollbar_color=None,
+                      input_elements_background_color='#F7F3EC',
+                      button_color=('white', '#4F4F4F'))
+
+        print(informationPassword)
+
+        login = [
+            [sg.Text('Alteração de Acesso\n')],
+            [sg.Text('Login '),
+             sg.Input(key='KEY_login', size=(20, 0))],
+            [
+                sg.Text('Senha'),
+                sg.Input(key='KEY_password', password_char='*', size=(20, 0))
+            ],
+        ]
+        button = [[
+            sg.Button('Confirmar', key='KEY_Confirm', size=(10, 0)),
+            sg.Button('Cancelar', key='KEY_Cancelar', size=(7, 0))
+        ]]
+
+        layout = [[sg.Column(login, element_justification='center')],
+                  [sg.Column(button, element_justification='center')]],
+
+        window = sg.Window('Acesso',
+                           layout,
+                           element_justification='center',
+                           size=(300, 200))
+
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED:
+                break
+
+            if 'KEY_Confirm' == event:
+                self.informationPassword = {
+                    **informationPassword, validationAlt:
+                    [values['KEY_login'], values['KEY_password']]
+                }
+                window.close()
+
+            if 'KEY_Cancelar' == event:
+                break
+
+            return informationPassword
+
+
+iniciaBoot = Interface()
+iniciaBoot.main()
--- main.py
+++ main.py
@@ -1,66 +1,76 @@
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-from interface import 
+from interface import Interface
 import time
 
 
-class OpenAbasBoot:
+class Boot:
     def __init__(self):
+        self.Interface = Interface.main()
+
         self.driver = webdriver.Firefox(
             executable_path=r'/usr/local/bin/geckodriver')
         self.condition = 0
 
-    def Acesso(self, condition):
+    def Controladora(self):
+        Interface = self.Interface
+
+        if Interface['Sgo'] == True:
+            self.sgo()
+            time.sleep(10)
+        if Interface['Linkedin'] == True:
+            self.linkedin()
+            time.sleep(10)
+        if Interface['Rocket'] == True:
+            self.rocket()
+            time.sleep(10)
+        if Interface['Email'] == True:
+            self.email()
+            time.sleep(10)
+        if Interface['Youtube'] == True:
+            self.youtube()
+            time.sleep(10)
+
+    def acesso(self, condition):
 
         driver = self.driver
         condition = self.condition
 
         if condition == 1:
-            acesso = 'gabriel.cunha'
+            acesso = 'Gabriel.cunha'
             return acesso
-        elif condition == 2:
+        if condition == 2:
             senha = 'Neto2133@'
             return senha
 
-        elif condition == 3:
-            acesso = 'gabriel.cunha'
-            return acesso
-        elif condition == 4:
-            senha = 'Neto2133@'
-            return senha
-
-    def Sgo(self):
-
+    def sgo(self):
         driver = self.driver
-
         driver.get('https://sgo.basis.com.br/')
-
-        login = 'gabriel.cunha'
-        password = 'Neto2133@'
+        #acesso = 'Gabriel.cunha'
+        #senha = 'Neto2133@'
 
         time.sleep(5)
-
         clickLogin = driver.find_element_by_xpath(
-            "//input[@id='username']").send_keys(login)
+            "//input[@id='username']").send_keys(acesso)
+
         clickSenha = driver.find_element_by_xpath(
-            "//input[@id='password']").send_keys(password)
+            "//input[@id='password']").send_keys(senha)
+
         clickButton = driver.find_element_by_xpath(
             "//input[@type='submit']").click()
-
-        self.rocket()
+        return
 
     def rocket(self):
-
         driver = self.driver
-        acesso = 'Gabriel.cunha'
-        senha = 'Neto2133@'
+        #acesso = 'Gabriel.cunha'
+        #senha = 'Neto2133@'
 
         driver.execute_script(
             "window.open('https://chat.basis.com.br', '_blank')")
 
-        driver.switch_to_window(driver.window_handles[1])
+        driver.switch_to.window(driver.window_handles[-1])
 
-        time.sleep(7)
+        time.sleep(10)
 
         clickLogin = driver.find_element_by_xpath(
             "//input[@id='emailOrUsername']").send_keys(acesso)
@@ -70,7 +80,72 @@ class OpenAbasBoot:
 
         clickButton = driver.find_element_by_xpath(
             "//button[@class='rc-button rc-button--primary login']").click()
+        return
+
+    def linkedin(self):
+        driver = self.driver
+        #acesso = 'gabrielcatvalhoneto@gmail.com'
+        #senha = 'neto2133'
+
+        driver.execute_script(
+            "window.open('https://www.linkedin.com/login/pt?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin', '_blank')"
+        )
+        driver.switch_to.window(driver.window_handles[-1])
+
+        time.sleep(1)
+
+        clickLogin = driver.find_element_by_xpath(
+            "//input[@id='username']").send_keys(acesso)
+
+        clickSenha = driver.find_element_by_xpath(
+            "//input[@id='password']").send_keys(senha)
+
+        clickButton = driver.find_element_by_xpath(
+            "//button[@class='btn_primary--large from_button--floating']"
+        ).click()
+        return
+
+    def email(self):
+        driver = self.driver
+        driver.execute_script(
+            "window.open('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin', '_blank')"
+        )
+        driver.switch_to.window(driver.window_handles[-1])
+        time.sleep(2)
+        #email = 'gabriel.cunha@basis.com.br'
+        #senha = 'g25443215'
+        time.sleep(5)
+        clickinicie = driver.find_element_by_xpath(
+            "//input[@id='identifierId']").send_keys(email, Keys.ENTER)
+        time.sleep(2)
+        clicksenha = driver.find_element_by_xpath(
+            "//input[@class='whsOnd zHQkBf']").send_keys(senha, Keys.ENTER)
+        time.sleep(2)
+        return
+
+    def youtube(self):
+        driver = self.driver
+        driver.execute_script(
+            "window.open('https://www.youtube.com/', '_blank')")
+        driver.switch_to.window(driver.window_handles[-1])
+
+        time.sleep(5)
+        musica = 'Mc Ryan sp'
+        clickmusica = driver.find_element_by_xpath(
+            "//input[@id='search']").send_keys(musica)
+        time.sleep(2)
+        clickPesquisamusica = driver.find_element_by_xpath(
+            "//button[@id='search-icon-legacy']").click()
+        time.sleep(4)
+        clickseleciona = driver.find_element_by_xpath(
+            "//yt-formatted-string[@class='style-scope ytd-video-renderer']"
+        ).click()
+        time.sleep(6)
+        clickpular = driver.find_element_by_xpath(
+            "//button[@class='ytp-ad-skip-button ytp-button']").click()
+
+        return
 
 
-Inicie_Boot = OpenAbasBoot()
-Inicie_Boot.Sgo()
+iniciaBoot = Boot()
+iniciaBoot.Controladora()
